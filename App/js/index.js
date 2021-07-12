16a182f50e8e15e44427efab5800d2ef0e03c7f9
savannahpear <63019156+savannahpear@users.noreply.github.com>
2021-07-12 12:26:54 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -22,7 +22,7 @@ xui.Class('App', 'xui.Module',{
             
             append(
                 xui.create("xui.UI.Tabs")
-                .setHost(host,"xui_ui_tabs8")
+                .setHost(host,"tabs")
                 .setItems([
                     {
                         "id" : "Home",
@@ -47,80 +47,64 @@ xui.Class('App', 'xui.Module',{
                 })
             );
             
-            host.xui_ui_tabs8.append(
+            host.tabs.append(
                 xui.create("xui.UI.SVGPaper")
                 .setHost(host,"home_svg")
                 .setLeft("0em")
                 .setTop("0.1523809523809524em")
                 .setWidth("60.95238095238095em")
-                .setHeight("42.51428571428571em"),
+                .setHeight("41.75238095238095em"),
                 "Home"
             );
             
             host.home_svg.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"inputter")
+                .setLeft("20.571428571428573em")
+                .setTop("14.552380952380952em")
+                .setWidth("27em")
+                .setHeight("5em")
+                .setLabelSize("8em")
+            );
+            
+            host.home_svg.append(
                 xui.create("xui.UI.Image")
-                .setHost(host,"xui_ui_image5")
-                .setLeft("0.7619047619047619em")
-                .setTop("1.5238095238095237em")
-                .setWidth("59.42857142857143em")
-                .setHeight("5.40952380952381em")
+                .setHost(host,"home_choose")
+                .setLeft("0em")
+                .setTop("-0.0761904761904762em")
                 .setSrc("{/}Publication2.jpg")
             );
             
             host.home_svg.append(
                 xui.create("xui.UI.Image")
-                .setHost(host,"xui_ui_image6")
-                .setLeft("2.2857142857142856em")
-                .setTop("6.857142857142857em")
+                .setHost(host,"xui_ui_image41")
+                .setLeft("1.5238095238095237em")
+                .setTop("6.095238095238095em")
                 .setWidth("57.5em")
                 .setHeight("5.083333333333333em")
                 .setSrc("{/}Publication3.jpg")
             );
             
-            host.xui_ui_tabs8.append(
+            host.home_svg.append(
                 xui.create("xui.UI.Image")
-                .setHost(host,"xui_ui_image9")
+                .setHost(host,"xui_ui_image44")
                 .setLeft("6.095238095238095em")
-                .setTop("14.628571428571428em")
+                .setTop("13.714285714285714em")
                 .setWidth("14.857142857142858em")
                 .setHeight("7.0095238095238095em")
-                .setSrc("{/}next.jpg"),
-                "Home"
+                .setSrc("{/}pub2.jpg")
             );
             
-            host.xui_ui_tabs8.append(
-                xui.create("xui.UI.Input")
-                .setHost(host,"inputter")
-                .setLeft("17.523809523809526em")
-                .setTop("16.228571428571428em")
-                .setWidth("27em")
-                .setHeight("5em")
-                .setLabelSize("8em"),
-                "Home"
-            );
-            
-            host.xui_ui_tabs8.append(
+            host.home_svg.append(
                 xui.create("xui.UI.Block")
                 .setHost(host,"home_block")
                 .setLeft("0.16666666666666666em")
                 .setTop("26.5em")
-                .setWidth("59.96190476190476em")
+                .setWidth("64.83333333333333em")
                 .setHeight("19.166666666666668em")
                 .setZIndex(0)
                 .setBackground("#31081F"),
-                "Home"
-            );
-            
-            host.xui_ui_tabs8.append(
-                xui.create("xui.UI.Label")
-                .setHost(host,"pickCat")
-                .setLeft("4.333333333333333em")
-                .setTop("29em")
-                .setCaption("Pick a Category:")
-                .setFontColor("#FFFFFF")
-                .setFontSize("26px"),
-                "Home"
-            );
+                "Home");
             
             return children;
             // ]]Code created by CrossUI RAD Studio
